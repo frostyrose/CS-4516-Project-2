c82bf14cb3e3c9175ead1e26bc78f86fbbb4ec91
JRWatson2013 <JRWatson@wpi.edu>
2015-12-02 18:43:36 -0500

    Fixed Catalog

--- Youtube-Miner/src/catalog.java
+++ Youtube-Miner/src/catalog.java
@@ -1,21 +1,17 @@
 import java.util.ArrayList;
 import java.util.HashMap;
 
-public class catalog {
+public class Catalog {
 	HashMap<String, Integer> regions = new HashMap(300);
 	
-	public void addval(ArrayList<String>codes){
+	public void addval(String key){
 		int valEdit;
-		String key;
-		for(int i = 1; i< codes.size(); i++) {
-		key = codes.get(i - 1);
 		if (regions.containsKey(key) == true){
 			valEdit = (int) regions.get(key);
 			valEdit = valEdit + 1;
 			regions.put(key, valEdit);
 		} else {
 			regions.put(key, 1);
-		}
 		}
 	}
 	public int rtnval(String key){
